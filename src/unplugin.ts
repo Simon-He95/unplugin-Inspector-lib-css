d22d4d4b1094149c7c944d1d3578df398e9d9b1f
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-09-01 09:56:39 +0800

    fix: 兼容vite版本导致的错误

--- src/unplugin.ts
+++ src/unplugin.ts
@@ -27,25 +27,26 @@ const unplugin = createUnplugin((options: Options = {}): any => {
         handler(options: any, bundle: any) {
           if (!styles)
             return
-          const outputPath = options.file || path.resolve(options.dir, options.entryFileNames)
+
+          const outputPath = options.file || path.resolve(options.dir, typeof options.entryFileNames === 'string' ? options.entryFileNames : options.entryFileNames(options.chunkFileNames))
           const cssCode = JSON.stringify(styles.replace(/\n/g, ' '))
           const insertStyle = `try{if(typeof document != 'undefined'){var elementStyle = document.createElement('style');elementStyle.appendChild(document.createTextNode(${cssCode}));document.head.appendChild(elementStyle);}}catch(e){console.error('vite-plugin-Inspector-lib-css', e);}`
           const { code } = Object.values(bundle)[0] as any
           fs.writeFile(outputPath, `${insertStyle}
-  ${code}`, 'utf-8')
+          ${code}`, 'utf-8')
         },
       },
-      transform: {
-        order: 'pre',
-        handler(code: string) {
-          if (!code.startsWith('// @unocss-include'))
-            return code
-          createGenerator({}, defaultConfig).generate(code || '').then((result) => {
-            const css = result.getLayers()
-            if (!styles.includes(css))
-              styles += `${css} `
+      transform(code: string) {
+        createGenerator({}, defaultConfig).generate(code || '').then((result) => {
+          const match = result.getLayers().match(/\/\*\s*layer\:\s*default\s*\*\/\n(.*)/ms)
+          if (!match)
+            return
+          const css = match[1]
+          css.split('}\n').forEach((s) => {
+            if (!styles.includes(s))
+              styles += `${css}} `
           })
-        },
+        })
       },
     },
   ]
